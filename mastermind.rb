08848bf7e66231a676ee36648468456066968577
Martyn <martynj2010@icloud.com>
2017-12-16 19:58:39 +0000

    First attempt at #compare function. Not tested.

--- mastermind.rb
+++ mastermind.rb
@@ -6,18 +6,31 @@ module Mastermind
     attr_accessor :row, :feedback
 
     def initialize
-      @feedback = Struct.new(:one, :two, :three, :four)
+      @feedback = Array.new(4)
     end
 
     def create_code (pegs)
-      @row = {one: pegs[0], two: pegs[1], three: pegs[2], four: peg[4]}
+      @row = {one: pegs[0], two: pegs[1], three: pegs[2], four: peg[3]}
     end
 
-    def compare_codes (master, guess)
+    def compare (g_row)
+      #paramenter is a hash representing a row.
+      @row.each do |pos, color|
+        if g_row[pos] == color
+          @feedback << "BLACK"
+        elsif g_row.include?(color)
+          @feedback << "WHITE"
+        end
+        @feedback.sort
+      end
 
     end
 
-    def feedback
+    def feedback(loc, type)
+
+
+
+      end
 
     end
 
@@ -25,18 +38,18 @@ module Mastermind
 
   class Code_Master < Code
 
-    @@peg = nil
-
     def initialize
       create_code
     end
 
-    def create_code # overide parent
+    def create_code # overide superclass method
       #Randonly generate a 4 peg code, assign a colour and postion (1 - 4)
       already_used = true
       pegs = Array.new
       pegs << get_color
+      count = 0
       3.times do
+        count += 1
         while already_used
           new_color = get_color
           pegs.include?(new_color) ? already_used = true : already_used = false
@@ -44,7 +57,7 @@ module Mastermind
         pegs << new_color
         already_used = true
       end
-      @row = {one: pegs[0], two: pegs[1], three: pegs[2], four: pegs[4]}
+      @row = {one: pegs[0], two: pegs[1], three: pegs[2], four: pegs[3]}
     end
 
     def get_color
