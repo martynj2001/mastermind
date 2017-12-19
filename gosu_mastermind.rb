4458067df942b9d42467ef4f704071547a0dbbc6
Martyn <martynj2010@icloud.com>
2017-12-19 20:17:54 +0000

    Started GOSU window for mastermind game. Added Media folder.

---
+++ gosu_mastermind.rb
@@ -0,0 +1,27 @@
+require 'gosu'
+
+module ZOrder
+  BACKGROUND, GRID, PEGS, UI = *0..3
+end
+
+class Mastermind < Gosu::Window
+
+  def initialize
+    super 640, 480
+    self.caption = "Mastermind"
+    @background_image = Gosu::Image.new("media/grey_background.gif", :tileable => true)
+    @font = Gosu::Font.new(30)
+  end
+
+  def update
+
+  end
+
+  def draw
+    @background_image.draw(0,0,ZOrder::BACKGROUND)
+    @font.draw("Mastermind", 250, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::BLACK)
+  end
+
+end
+
+Mastermind.new.show
