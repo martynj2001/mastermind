6759ca41048f4089e8a4089c34227c8060884a6d
Martyn <martynj2010@icloud.com>
2017-12-27 21:23:47 +0000

    Added feedback images (black and white pegs). After selecting code and pressing enter, code is displayed on new line on board.

--- gosu_mastermind.rb
+++ gosu_mastermind.rb
@@ -1,5 +1,5 @@
   require 'gosu'
-
+  require './mastermind.rb'
 
   module ZOrder
     BACKGROUND, GRID, PEGS, UI = *0..3
@@ -15,7 +15,7 @@
     YELLOW_X, YELLOW_Y = 520, 350
 
 
-    attr_accessor :prompt_text, :guess, :peg_selected, :pegs
+    attr_accessor :prompt_text, :guess, :peg_selected, :pegs, :history, :master_code
 
     def initialize
       super 640, 480
@@ -27,12 +27,18 @@
       @blue_peg = Gosu::Image.new("media/blue_dot.png", :tileable => true)
       @green_peg = Gosu::Image.new("media/green_dot.png", :tileable => true)
       @yellow_peg = Gosu::Image.new("media/yellow_dot.png", :tileable => true)
+      @white_sq = Gosu::Image.new("media/white_sq.png", :tileable => true)
+      @black_sq = Gosu::Image.new("media/black_sq.png", :tileable => true)
+
       @title_font = Gosu::Font.new(32, name: "Nimbus Mono L")
       @peg_image = Array.new
+      @pegs = Array.new
+      @history = Array.new
       @prompt_text = "Welcome to Mastermind..... Press Enter to continue."
       @prompt, @guess_no = Gosu::Font.new(20, name: "Nimbus Mono L")
       @guess = 0
       @peg_selected = false
+      @master_code = Code_Master.new
 
     end
 
@@ -57,16 +63,24 @@
       @green_peg.draw(GREEN_X, GREEN_Y, ZOrder::PEGS)
       @yellow_peg.draw(YELLOW_X, YELLOW_Y, ZOrder::PEGS)
 
+      # Draw any previous attempts on a new line.
+      if @history.length > 0 
+        line_y = 325
+        @history.each do |line|
+          line_x = 175
+          line_y -= 45
+          line.each {|img| img.draw(line_x += 50, line_y, ZOrder::PEGS)}
+        end
+      end
+
       if peg_selected
-        puts "Pegs = #{@peg_image.length}"
-        peg_x = 200
+        peg_x = 175
           @peg_image.each do |img|
             unless img == nil || peg_x >= 400
               img.draw(peg_x += 50, 350, ZOrder::PEGS)
             end
           end
       end
-
       @title_font.draw("Mastermind", 250, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::BLACK)
     end
 
@@ -77,6 +91,23 @@
           @guess += 1
         else
           @prompt_text = "You have had #{@guess} attempts"
+          unless @peg_image.length < 4
+            player = Code.new
+            puts "Code Object Created"
+            player.create_code(@pegs)
+            print player.row
+            @master_code.compare(player)
+
+            if player.has_won?
+              @prompt_text = "Congratulations you have broken the code in #{@guess} turns."
+            else
+              #need to display feedback.
+              @peg_selected = false
+              @history.push << @peg_image.dup
+              print @history
+              @peg_image.clear
+            end
+          end
           @guess += 1
         end
       elsif id == Gosu::MS_LEFT
@@ -90,19 +121,23 @@
       if @peg_image.length < 4
         if mouse_x.to_i.between?(RED_X - DOT_WIDTH, RED_X + DOT_WIDTH) && mouse_y.to_i.between?(RED_Y - DOT_HEIGHT, RED_Y + DOT_HEIGHT)
           @peg_image << @red_peg
+          @pegs << "Red"
           puts "Red selected and clicked"
         elsif mouse_x.to_i.between?(BLUE_X - DOT_WIDTH, BLUE_X + DOT_WIDTH) && mouse_y.to_i.between?(BLUE_Y - DOT_HEIGHT, BLUE_Y + DOT_HEIGHT)
           @peg_image << @blue_peg
           puts "Blue selcted and clicked"
+          @pegs << "Blue"
         elsif mouse_x.to_i.between?(GREEN_X - DOT_WIDTH, GREEN_X + DOT_WIDTH) && mouse_y.to_i.between?(GREEN_Y - DOT_HEIGHT, GREEN_Y + DOT_HEIGHT)
           @peg_image << @green_peg
           puts "Green selected and clicked"
+          @pegs << "Green"
         elsif mouse_x.to_i.between?(YELLOW_X - DOT_WIDTH, YELLOW_X + DOT_WIDTH) && mouse_y.to_i.between?(YELLOW_Y - DOT_HEIGHT, YELLOW_Y + DOT_HEIGHT)
           @peg_image << @yellow_peg
           puts "Yellow selected and clicked"
+          @pegs << "Yellow"
         end
-        @peg_selected = true
       end
+      @peg_selected = true
     end
 
   end
--- mastermind.rb
+++ mastermind.rb
@@ -1,81 +1,83 @@
 # Mastermind Game - The Odin TheOdinProject Intermediate Ruby Project 2
-module Mastermind
 
-  class Code
 
-    attr_accessor :row, :correct_code, :black, :white
+class Code
 
-    def initialize
-      @black = 0
-      @white = 0
-      @correct_code = false
-    end
+  attr_accessor :row, :correct_code, :black, :white
 
-    def create_code (pegs)
-      @row = {one: pegs[0], two: pegs[1], three: pegs[2], four: pegs[3]}
-    end
+  def initialize
+    @black = 0
+    @white = 0
+    @correct_code = false
+  end
 
-    def compare (code)
-      #paramenter is a hash representing a row.
-      puts "Comparing Codes ......"
-      @row.each do |pos, color|
-        puts "Position #{pos} is #{color}"
-        if code.row.has_value?(color)
-          if pos == code.row.key(color)
-            code.black += 1
-          else
-            code.white += 1
-          end
-        end
-      end
-    end
+  def create_code (pegs)
+    @row = {one: pegs[0], two: pegs[1], three: pegs[2], four: pegs[3]}
+  end
 
-    def has_won?
-      @black == 4 ? true : false
-    end
-
-  end # Code
-
-  class Code_Master < Code
-
-    def initialize
-      create_code
-      @feedback = Array.new(4, " ")
-      @correct_code = false
-    end
-
-    def create_code # overide superclass method
-      #Randonly generate a 4 peg code, assign a colour and postion (1 - 4)
-      already_used = true
-      pegs = Array.new
-      pegs << get_color
-      count = 0
-      3.times do
-        count += 1
-        while already_used
-          new_color = get_color
-          pegs.include?(new_color) ? already_used = true : already_used = false
-        end
-        pegs << new_color
-        already_used = true
-      end
-      @row = {one: pegs[0], two: pegs[1], three: pegs[2], four: pegs[3]}
-    end
-
-    def get_color
-      col = rand(1..4)
-      case col
-        when 1
-          "Red"
-        when 2
-          "Blue"
-        when 3
-          "Green"
+  def compare (code)
+    #paramenter is a hash representing a row.
+    puts "Comparing Codes ......"
+    @row.each do |pos, color|
+      puts "Position #{pos} is #{color}"
+      if code.row.has_value?(color)
+        if pos == code.row.key(color)
+          code.black += 1
         else
-          "Yellow"
+          code.white += 1
         end
       end
+    end
+  end
 
-      private :create_code, :get_color
-  end # Code
-end #Mastermind
+  def has_won?
+    @black == 4 ? true : false
+  end
+
+end # Code
+
+class Code_Master < Code
+
+  def initialize
+    create_code
+    @feedback = Array.new(4, " ")
+    @correct_code = false
+    @black = 0
+    @white = 0
+  end
+
+  def create_code # overide superclass method
+    #Randonly generate a 4 peg code, assign a colour and postion (1 - 4)
+    already_used = true
+    pegs = Array.new
+    pegs << get_color
+    count = 0
+    3.times do
+      count += 1
+      while already_used
+        new_color = get_color
+        pegs.include?(new_color) ? already_used = true : already_used = false
+      end
+      pegs << new_color
+      already_used = true
+    end
+    @row = {one: pegs[0], two: pegs[1], three: pegs[2], four: pegs[3]}
+    print @row
+  end
+
+  def get_color
+    col = rand(1..4)
+    case col
+      when 1
+        "Red"
+      when 2
+        "Blue"
+      when 3
+        "Green"
+      else
+        "Yellow"
+      end
+    end
+
+    private :create_code, :get_color
+end # Code
